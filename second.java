4db88785948a1b7ea77d0c34a737d631ed4a1397
Sanduni Nisansala <139616844+Sanduni0601@users.noreply.github.com>
2025-03-16 09:36:23 +0000

    Answer question02

---
+++ second.java
@@ -0,0 +1,11 @@
+//2)print numbers from 1 to 10
+
+import java.util.*;
+
+public class second{
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);  
+        }
+    }
+}
