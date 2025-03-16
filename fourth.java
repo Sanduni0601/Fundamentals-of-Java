04f8a1cacdb7e3684f42bbfaf8e2b2452acdc5bc
Sanduni Nisansala <139616844+Sanduni0601@users.noreply.github.com>
2025-03-16 09:46:58 +0000

    Answer question04

---
+++ fourth.java
@@ -0,0 +1,10 @@
+//4)print the odd numbers between 1 to 20
+public class fourth {
+    public static void main(String[] args){
+        for(int i=1;i<=20;i++){
+            if(i%2==1){
+                System.out.println(i);
+            }
+        }
+    }
+}
